ad4b866c9b21c78220d54de6b9b406f71097144c
ass1or <ass1or@tuta.com>
2026-03-15 18:56:41 +0100

    new table

--- src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/HealthMetrics.java
@@ -1,48 +1,94 @@
 package pl.wsb.fitnesstracker.healthmetrics;
 
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 import pl.wsb.fitnesstracker.user.api.User;
 
 import java.time.LocalDate;
 
+
 @Entity
 @Table(name = "health_metrics")
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
 public class HealthMetrics {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Nullable
     private Long id;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column(nullable = false)
+    @Column(name = "date", nullable = false)
     private LocalDate date;
 
-    @Column(nullable = false)
-    private double weight;
+    @Column(name = "weight")
+    private Double weight;
 
-    @Column(nullable = false)
-    private double height;
+    @Column(name = "height")
+    private Double height;
 
-    @Column(name = "heart_rate", nullable = false)
-    private int heartRate;
+    @Column(name = "heart_rate")
+    private Integer heartRate;
 
-    public HealthMetrics(User user, LocalDate date, double weight, double height, int heartRate) {
+
+    public HealthMetrics() {
+    }
+
+
+    public HealthMetrics(User user, LocalDate date, Double weight, Double height, Integer heartRate) {
         this.user = user;
         this.date = date;
         this.weight = weight;
         this.height = height;
+        this.heartRate = heartRate;
+    }
+
+    // Gettery i Settery
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Integer getHeartRate() {
+        return heartRate;
+    }
+
+    public void setHeartRate(Integer heartRate) {
         this.heartRate = heartRate;
     }
 }
