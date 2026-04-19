3b21d147a3c51e0ed92eab586bb662e6fa3dc15c
ass1or <ass1or@tuta.com>
2026-04-19 11:33:35 +0200

    lab03

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,16 @@
 package pl.wsb.fitnesstracker.event;
 
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "event")
+@Getter
+@Setter
+@NoArgsConstructor
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -10,5 +21,14 @@ import jakarta.persistence.Table;
 public class Event {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String location;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDate startDate;
 }
