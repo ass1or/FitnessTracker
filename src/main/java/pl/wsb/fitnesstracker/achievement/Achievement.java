1fe0614aadfc300522214d1b2d099908ebe55840
ass1or <ass1or@tuta.com>
2026-04-19 18:09:32 +0200

    lab03 - wersja B

---
+++ src/main/java/pl/wsb/fitnesstracker/achievement/Achievement.java
@@ -0,0 +1,30 @@
+package pl.wsb.fitnesstracker.achievement;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.wsb.fitnesstracker.user.api.User;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "achievements")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Achievement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "earned_at", nullable = false)
+    private LocalDateTime earnedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
